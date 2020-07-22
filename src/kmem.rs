72bf3953dc06439c818678fdcc7141875fea6867
Daniel Boyle <boylede+github@gmail.com>
2020-07-21 19:11:41 -0700

    add safety notes, change pointer size per clippy

--- src/kmem.rs
+++ src/kmem.rs
@@ -11,22 +11,27 @@ static mut KMEM_HEAD: *mut AllocList = null_mut();
 /// MMU page table for kernel
 static mut KMEM_PAGE_TABLE: *mut PageTable = null_mut();
 
+#[cfg(target_pointer_width = "64")]
+const BYTES_PER_USIZE: usize = 8;
+#[cfg(target_pointer_width = "32")]
+const BYTES_PER_USIZE: usize = 4;
+
 /// Safe wrapper around page table global
 pub fn get_page_table() -> &'static mut PageTable {
     unsafe {
-    // SAFETY: we are converting a mutable pointer to a mutable reference,
-    // we need to ensure that the pointer is null, or all of the following
-    // are true of the memory location & range:
-    // a) initialized, b) valid for PageTable, c) properly aligned for
-    // PageTable, d) non-null, e) contain a single allocated object f) no
-    // other access to this location occurs during the lifetime
-    // of the mutable reference we are creating.
-    // a: page table was zero'd when allocated
-    // b: global symbol is the correct size, alignment for pagetable as the pointer was declared with this same type
-    // c: same
-    // d: non-null per a
-    // e: does not overlap with any other object, as we ensure through our page-grained allocation
-    // f: we will ensure single access in the future
+        // SAFETY: we are converting a mutable pointer to a mutable reference,
+        // we need to ensure that the pointer is null, or all of the following
+        // are true of the memory location & range:
+        // a) initialized, b) valid for PageTable, c) properly aligned for
+        // PageTable, d) non-null, e) contain a single allocated object f) no
+        // other access to this location occurs during the lifetime
+        // of the mutable reference we are creating.
+        // a: page table was zero'd when allocated
+        // b: global symbol is the correct size, alignment for pagetable as the pointer was declared with this same type
+        // c: same
+        // d: non-null per a
+        // e: does not overlap with any other object, as we ensure through our page-grained allocation
+        // f: we will ensure single access in the future
         KMEM_PAGE_TABLE.as_mut().unwrap()
     }
 }
@@ -85,17 +90,16 @@ pub fn setup() {
         // items once at startup and then they are immutable. in the
         // future, we can add some protection.
         KMEM_ALLOC = 512;
-        let k_alloc = zalloc(KMEM_ALLOC);
-        assert!(!k_alloc.is_null());
+        let k_alloc = zalloc(KMEM_ALLOC).unwrap();
         KMEM_HEAD = k_alloc as *mut AllocList;
         let kmem = KMEM_HEAD.as_mut().unwrap();
         kmem.set_free();
         kmem.set_size(KMEM_ALLOC * PAGE_SIZE);
-        KMEM_PAGE_TABLE = zalloc(1) as *mut PageTable;
+        KMEM_PAGE_TABLE = zalloc(1).unwrap() as *mut PageTable;
     }
 }
 
-pub fn kzmalloc(_size: usize) -> *mut u8 {
+pub fn kzmalloc(size: usize) -> *mut usize {
     let size = align_power(size, 3);
     let address = kmalloc(size);
     if !address.is_null() {
@@ -108,25 +112,29 @@ pub fn kzmalloc(_size: usize) -> *mut u8 {
     address
 }
 
-pub fn kmalloc(size: usize) -> *mut u8 {
+pub fn kmalloc(size: usize) -> *mut usize {
     // scale the size to 8-byte boundaries (lowest three bits zero)
     // and add space required to store metadata
     let size = align_power(size, 3) + size_of::<AllocList>();
 
     // local variable will be used to walk through the kernel memory space
     // one allocation at a time
-    let mut head = unsafe { 
-        // SAFETY: access to static global, 
+    let mut head = unsafe {
+        // SAFETY: access to static global,
         // we must ensure no one has mutable access to head
         // currently, we treat all the KMEM_ globals as only mutable during setup.
-        KMEM_HEAD 
+        KMEM_HEAD
     };
     let mut current_allocation = unsafe {
-        // SAFETY: 
+        // SAFETY:
         head.as_mut()
-    }.unwrap();
+    }
+    .unwrap();
     // local variable to compare to head while walking kernel memory
-    let tail = unsafe { (head as *mut u8).add(KMEM_ALLOC * PAGE_SIZE) } as *mut AllocList;
+    let tail = unsafe {
+        // SAFETY: pointer arithmatic. alignment is known correct because we start with a usize aligned pointer and alloclist is aligned like usize
+        (head as *mut usize).add(KMEM_ALLOC * PAGE_SIZE / BYTES_PER_USIZE) as *mut AllocList
+    };
     while head < tail {
         if current_allocation.is_free() && size <= current_allocation.get_size() {
             // split this chunk and return
@@ -151,7 +159,7 @@ pub fn kmalloc(size: usize) -> *mut u8 {
                 current_allocation.set_size(chunk_size);
             }
             // offset pointer by size of the metadata and coerce to general pointer
-            return unsafe { head.add(1) } as *mut u8;
+            return unsafe { head.add(1) } as *mut usize;
         } else {
             // go to next chunk
             head =
