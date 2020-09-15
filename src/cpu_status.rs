8d027761e85456e07a26fe8bb37aea45f508329c
Daniel Boyle <boylede+github@gmail.com>
2020-09-14 19:34:55 -0700

    add symbol for set mstatus

--- src/cpu_status.rs
+++ src/cpu_status.rs
@@ -9,6 +9,7 @@ extern "C" {
     fn asm_get_mimpid() -> usize;
     fn asm_get_mhartid() -> usize;
     fn asm_get_mstatus() -> usize;
+    fn asm_set_mstatus(_:usize);
     fn asm_get_mepc() -> usize;
 }
 
@@ -114,7 +115,6 @@ fn get_base_width() -> u64 {
         } else {
             64
         }
-        
     }
 }
 
