55f46dcefbfe25651a88587e26d9f1b710070c5f
Daniel Boyle <boylede+github@gmail.com>
2023-01-07 22:01:41 -0500

    fix incorrect mpp value after return from kinit, add comments

--- src/assembly/boot.s
+++ src/assembly/boot.s
@@ -1,95 +1,94 @@
-# don't use compressed instructions
 .option norvc
-.section .data
+
 
 .section .text.init
+
+
 .global _start
 _start:
 
 .option push
-.option norelax # ensures the following assembly is not relaxed by the linker
+.option norelax
     # load the memory location at the end of the
     # text section / begining of rodata, per layout
-    la  gp, _global_pointer
-.option pop # reverts the norelax option to whatever was set before
+	la		gp, _global_pointer
+.option pop
 
-    # clear address translation, protection
-    # this is not needed, implementation should set this zero already
-    # more of a reference for the reader
-    csrw    satp, zero
+	# clear address translation
+	# technically not on anyway
+	csrw	satp, zero
 
-    # if core id != 0, jump to infinite loop
-    # we only run boot sequence on core 0,
-    # other cores will be woken later with an IPI
-    csrr    t0, mhartid
-    bnez    t0, 4f
-    
-    # clear .bss section
-    la      a0, _bss_start
-    la      a1, _bss_end
-    bgeu    a0, a1, 2f
+	# cores other than 0
+	# will park while we boot
+	csrr	t0, mhartid
+	bnez	t0, 3f
+
+	# zero bss section
+	la 		a0, _bss_start
+	la		a1, _bss_end
+	bgeu	a0, a1, 2f
+
 1:
-    sd      zero, (a0)
-    addi    a0, a0, 8
-    bltu    a0, a1, 1b
+	sd		zero, (a0)
+	addi	a0, a0, 8
+	bltu	a0, a1, 1b
+
+	# done zero-ing bss
 2:
-    # allow superviser-mode interrupt and exception handling
-    # todo: reenable somewhere?
-    #li      t5, 0xffff
-    #csrw    medeleg, t5
-    #csrw    mideleg, t5
+	# setup stack pointer per layout
+	la		sp, _stack_end
 
-    # set the stack pointer to end of stack space
-    la      sp, _stack_end
-    # set the mstatus register, MPP=3, MPIE=1, MIE=1
-    # li      t0, (0b11 << 11) | (1 << 7) | (1 << 3)
-    # actually, jk, just set mpp = 2
-    li      t0, (0b11 << 11)
-    csrw    mstatus, t0
-    
-
-    # li      t3, (1 << 3) | (1 << 7) | (1 << 11)
-    # csrw    mie, t3
-    
-    # disable interupts
-    csrw    mie, zero
-
-    # set MEPC (exception program counter) to kernel entry point
-    la      t1, kinit
-    csrw    mepc, t1
-
-    # set the address we will return to after kinit runs
-    la      ra, enter_supervisor
-    # mret will update mstatus as well
-    mret
-
-
-# setup to run after kinit has finished
-enter_supervisor:
-    # set mstatus previous-status bits to desired status
-    # MPP = 1 [supervisor], offset 11; mpie = 1, offset 7, spie = 1, offset 5
-    li		t0, (0b01 << 11) | (1 << 7) | (1 << 5)
+	# set previous MPP to Machine
+	# so mret won't pop us out of Machine privilege
+	# when we go to kinit
+	li		t0, 0b11 << 11
 	csrw	mstatus, t0
-    # set trap vector
-    la		t2, asm_trap_vector
-	csrw	mtvec, t2
-    # set mepc to the desired rust kernel main function, kmain
-    la		t1, kmain
+
+	# disable interupts
+	csrw	mie, zero
+
+	# set MEPC to kinit so we will return there
+	# on mret
+	la		t1, kinit
 	csrw	mepc, t1
 
-    # set mie
-    # li		t2, (1 << 1) | (1 << 5) | (1 << 9)
-	# csrw	mie, t2
-    li      t3, (1 << 3) | (1 << 7) | (1 << 11)
-    csrw    mie, t3
-    # set ra to where we will come back to when kmain finishes.
-    la      ra, 4f
-    # use mret, which moves previous-status to current status bits
-    # and moves instruction pointer back into rust code
-    mret
+	# set return pointer so we come back here
+	# when kinit returns 
+	la		ra, 2f
 
+	# call kinit
+	mret
+2:
+	# again, set MPP to machine
+	# but also MIE and MPIE to 1
+	# to enable machine interrupts,
+	# before and after mret
+	li		t0, (11 << 11) | (1 << 7) | (1 << 5)
+	csrw	mstatus, t0
 
-# infinite loop
-4:
-    wfi
-    j   4b
+	# set machine trap vector
+	la		t2, asm_trap_vector
+	csrw	mtvec, t2
+
+	# setup MEPC to kmain as before
+	la		t1, kmain 
+	csrw	mepc, t1
+
+	# enable external, timer, and software 
+	# interrupts only
+	li		t2, 0x888
+	csrw	mie, t2
+
+	# set return pointer so we come back here
+	# when kmain returns 
+	la		ra, 3f
+
+	# call kmain
+	mret
+
+# initialize non-zero cores
+# and park
+3:
+	wfi
+	j		4b
+
