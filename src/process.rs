ee2e50408e2b090d0b72e14ad3bc7e4187f45093
Dan Boyle <boylede@gmail.com>
2023-09-09 17:46:17 -0400

    revert process to stub

--- src/process.rs
+++ src/process.rs
@@ -3,7 +3,6 @@ use fiveos_riscv::mmu::page_table::untyped::PageTableUntyped;
 use fiveos_riscv::mmu::page_table::PAGE_SIZE;
 use fiveos_riscv::mmu::EntryFlags;
 
-use crate::memory::allocator::page::{alloc, zalloc};
 use crate::trap::TrapFrame;
 
 // todo: replace with atomic increment
@@ -36,33 +35,6 @@ pub struct Process {
 
 impl Process {
     pub fn new(func: fn(), descriptor: &PageTableDescriptor) -> Option<Process> {
-        let func = func as usize;
-        let id = unsafe {
-            let id = PID_COUNTER;
-            PID_COUNTER += 1;
-            id
-        };
-        let stack = alloc(STACK_SIZE)? as *mut u8;
-        let stack_pointer = unsafe { stack.add(PAGE_SIZE * STACK_SIZE) };
-        let page_table = zalloc(1)? as *mut PageTableUntyped;
-        unsafe {
-            let table = page_table.as_mut().unwrap();
-            table.map(descriptor, 0, 0, 0, EntryFlags::USER_READ_WRITE, zalloc);
-        }
-
-        let mut trap_frame = TrapFrame::zero();
-        trap_frame.regs[2] = stack_pointer as usize;
-
-        let page_table = page_table as *mut u8;
-        let process = Process {
-            id,
-            state: Status::Created,
-            instruction_pointer: func,
-            stack_pointer,
-            page_table,
-            trap_frame,
-        };
-
-        Some(process)
+        todo!()
     }
 }
